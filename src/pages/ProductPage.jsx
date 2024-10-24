87a64343ef4890fb676dda6c001dfb59f51b2486
Xhanina <166377712+XhaninaIna@users.noreply.github.com>
2024-10-24 10:56:53 +0200

    Update ProductPage.jsx

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,35 +1,33 @@
-
-import { useSearchParams , useLocation, useParams} from 'react-router-dom';
-import { useEffect, useState } from 'react';
-// i kalojme te dhenat nga historia e state 
+import { useSearchParams, useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "./productPage.css";
+// i kalojme te dhenat nga historia e state
 export default function ProductPage() {
-    //query strings
-    const [searchParams, setSearchParams]= useSearchParams();
-    console.log(searchParams);
-    // marrim te dhenat nga historia e state me locationin   
-    const location = useLocation();
-    // marrim emrin me useParams
-    const {productSlug}=useParams();
-    // state qe marrim produktin
-    const [product, setProduct] = useState(null);
-    
+  //query strings
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams);
+  // marrim te dhenat nga historia e state me locationin
+  const location = useLocation();
+  // marrim emrin me useParams
+  const { productSlug } = useParams();
+  // state qe marrim produktin
+  const [product, setProduct] = useState(null);
 
-    // mbushim product me vlera qe ka state
-    useEffect(() => {
-        console.log(location.state);
-        if (location.state!==null) {
-            setProduct(location.state);
-        }
-    }, [location]);
-
-   
-    return (
-        <div>
+  // mbushim product me vlera qe ka state
+  useEffect(() => {
+    console.log(location.state);
+    if (location.state !== null) {
+      setProduct(location.state);
+    }
+  }, [location]);
+  return (
+    <div className="product-page">
       <p> Id : {product?.id} </p>
       <p> Name : {productSlug}</p>
       <p>Description:{product?.description}</p>
-      <p> price: {product?.price}</p>
-      <p> Path: {product?.path}</p>
-        </div>
-    );
+      <p>Price: {product?.price}</p>
+      <p>Path: {product?.path}</p>
+      <p>Color:{product?.color}</p>
+    </div>
+  );
 }
