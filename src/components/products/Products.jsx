d92bb25b195a5af3da343d0d6e50fc873a57957d
Xhanina <166377712+XhaninaIna@users.noreply.github.com>
2024-10-24 10:55:58 +0200

    Update Products.jsx

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
-
-export default function Products({id, name, description, price}){
-    const navigate= useNavigate();
-   
-    return (
-    <div>
-        <p>Id: {id}</p>
-        <p>Name: {name}</p>
-        <p>Description: {description}</p>
-        <p>Price: {price}</p>
-        <button onClick={()=>navigate(`/store/${name}`, {state: {id, description, price, path:`/store/${name}`}})}>Show More</button>
-        {/* id e kalojme sipas state */}
+import { useNavigate } from "react-router-dom";
+import "./products.css";
+export default function Products({ id, name, description, price, color }) {
+  const navigate = useNavigate();
+  return (
+    <div className="product-card">
+      <p className="product-id">Id: {id}</p>
+      <h2 className="product-name">Name:{name}</h2>
+      <p className="product-description">Description:{description}</p>
+      <p className="product-price">Price:{price}</p>
+      <p className="product-color">Color: {color}</p>
+      <button
+        className="product-button"
+        onClick={() =>
+          navigate(`/store/${name}`, {
+            state: { id, description, price, path: `/store/${name}`,color },
+          })
+        }
+      >
+        Show More
+      </button>
     </div>
-    );
+  );
 }
